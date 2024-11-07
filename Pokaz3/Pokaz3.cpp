8f6d5ead4b74ca2f79265657d60b07cfa70dfb2b
olegk <kbryzhenyukpc@gmail.com>
2024-11-07 10:22:49 +0200

    Добавьте файлы проекта.

---
+++ Pokaz3/Pokaz3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+void copyReversedArray(int* src, int* dest, int size) {
+    int* srcEnd = src + size - 1;
+    for (int i = 0; i < size; i++) {
+        *(dest + i) = *(srcEnd - i);
+    }
+}
+
+int main() {
+    int size = 5;
+    int srcArray[] = { 1, 2, 3, 4, 5 };
+    int* destArray = new int[size];
+
+    copyReversedArray(srcArray, destArray, size);
+
+    cout << "массив задом наперёд ";
+    for (int i = 0; i < size; i++) {
+        cout << destArray[i] << " ";
+    }
+    cout << endl;
+
+    delete[] destArray;
+    return 0;
+}
